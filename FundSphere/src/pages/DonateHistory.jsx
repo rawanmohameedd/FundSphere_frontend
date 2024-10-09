2f9be161a07d9678330031b94dcfa23b358f2fab
rawanmohameedd <nrawana12345@gmail.com>
2024-10-09 08:04:30 +0300

    final adjustments

--- FundSphere/src/pages/DonateHistory.jsx
+++ FundSphere/src/pages/DonateHistory.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Donations } from '../Components/Donations'
+import { server } from '../server'
 
 export const DonateHistory = () => {
     const campaign_id = useParams()
     const id = Number(campaign_id.campaign_id)
     console.log(id)
-  return (
-    <div>DonateHistory</div>
-  )
+
+    const [donations, setDonations] = useState([])
+    useEffect(() => {
+        const fetchDonations = async () => {
+            try {
+                const response = await axios.get(`${server}/campaignDonations/${id}`)
+                setDonations(response.data)
+                console.log(response.data)
+            } catch (err) {
+                console.error("can't get user donations", err.message)
+            }
+        }
+        fetchDonations()
+    }, [id])
+    return (
+        <>
+            {donations ?(
+                <div>
+                    <Donations donations={donations} />
+                </div>
+            ):(
+            <div>
+                This campaign hasn't had any donations yet
+            </div>
+            )}
+        </>
+    )
 }
--- FundSphere/src/pages/Profile.jsx
+++ FundSphere/src/pages/Profile.jsx
@@ -11,6 +11,7 @@ import { handleFullscreen } from '../Components/fullscreen';
 import { Campaigns } from '../Components/campaigns';
 import Popup from '../Components/popup';
 import { Footer } from '../Components/footer';
+import { Donations } from '../Components/Donations';
 
 export const Profile = () => {
     const [profile, setProfile] = useState(null);
@@ -223,30 +224,7 @@ export const Profile = () => {
                         Your Donations {donations ? <FaChevronUp /> : <FaChevronDown />}
                     </h2>
                     <div className={` ${donations ? 'max-h-full' : 'max-h-0'} overflow-hidden `}>
-                        <div className='flex flex-wrap  justify-center mt-[10px] p-[20px] space-x-5'>
-                            {userDonations &&
-                                userDonations.map((donation) => {
-                                    const id = donation.donation_id
-                                    const date = new Date(donation.donation_date).toISOString().split('T')[0]
-                                    const image = donation.campaign.campaign_photo;
-
-                                    return (
-                                        <div key={id} className='flex flex-col flex-wrap space-y-4 space-x-4 w-[40%] md:w-[90%] items-center justify-between mb-4 p-4 border border-gray-200 rounded-lg hover:bg-color1'>
-                                            <Link to={`/campain/${donation.campaign_id}`} className='flex flex-1 text-color2  hover:text-inherit space-x-2'>
-                                                <p className='flex items-center justify-center'>
-                                                    You donate {donation.amount} for {donation.campaign.title} at {date}
-                                                </p>
-                                                <div className='flex items-center justify-center'>
-                                                    <img src={image} alt={donation.campaign.title} className='h-[150px] w-[150px] object-contain' />
-                                                </div>
-
-                                            </Link>
-                                        </div>
-                                    )
-                                })
-
-                            }
-                        </div>
+                        <Donations donations={userDonations}/>
                     </div>
                 </section>
             </div>
